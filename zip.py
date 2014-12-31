687303893183d156f15352ff536ad32e4e1f5c4c
javnik36 <javnik36@gmail.com>
2014-12-31 13:55:28 +0100

    adding compression in zip.py

--- zip.py
+++ zip.py
@@ -1,9 +1,19 @@
-import zipfile
+def zip(path="/"):
+    '''(path="/")-->gtfs.zip
+    Zipuje wszystkie pliki potrzebne do gtfs'a.
+    '''
+    import zipfile
+    import zlib
+    import os.path as sys
 
-files = ["agency.txt", "calendar.txt", "feed_info.txt", "routes.txt", "stop_times.txt", "stops.txt", "trips.txt"]
-zf = zipfile.ZipFile("gtfs.zip", 'w')
+    files = ["agency.txt", "calendar.txt", "feed_info.txt", "routes.txt", "stop_times.txt", "stops.txt", "trips.txt"]
+    zf = zipfile.ZipFile("gtfs.zip", 'w', compression=zipfile.ZIP_DEFLATED)
 
-for file in files:
-    zf.write(file)
+    for file in files:
+        if sys.isfile(file):
+            zf.write(file)
+        else:
+            print("Brakuje pliku " + file + " w zipie gtfs!")
 
-zf.close()
+    zf.close()
+
