f013b278cb8fadf2ff98fd16b661b1372c393860
javnik36 <javnik36@gmail.com>
2014-12-31 19:56:06 +0100

    finishing the zip.py

--- zip.py
+++ zip.py
@@ -1,13 +1,18 @@
-def zip(path="/"):
-    '''(path="/")-->gtfs.zip
+def zip(path="/gtfs.zip"):
+    '''[files]-->gtfs.zip
     Zipuje wszystkie pliki potrzebne do gtfs'a.
     '''
     import zipfile
     import zlib
     import os.path as sys
 
+    if ".zip" not in path:
+        sciezka = path + "/gtfs.zip"
+    else:
+        sciezka = path
+
     files = ["agency.txt", "calendar.txt", "feed_info.txt", "routes.txt", "stop_times.txt", "stops.txt", "trips.txt"]
-    zf = zipfile.ZipFile("gtfs.zip", 'w', compression=zipfile.ZIP_DEFLATED)
+    zf = zipfile.ZipFile(sciezka, 'w', compression=zipfile.ZIP_DEFLATED)
 
     for file in files:
         if sys.isfile(file):
@@ -16,4 +21,3 @@ def zip(path="/"):
             print("Brakuje pliku " + file + " w zipie gtfs!")
 
     zf.close()
-
